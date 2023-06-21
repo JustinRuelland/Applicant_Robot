710a78c86a318e1e7c2f398c905ecba813d0e3c7
Justin RUELLAND <justinruelland@icloud.com>
2023-06-21 16:44:33 +0200

    MaJ scraper serp

--- files_uw/scraper_serp.py
+++ files_uw/scraper_serp.py
@@ -16,31 +16,32 @@ def link_scraper(links_list, driver):
             links_list.append(link.get_attribute("href"))
 
 def linkedin_link_scraper(links_list, URL):
-	#Ouverture du driver chrome
-	driver = webdriver.Chrome()
-	driver.get(URL)
-	time.sleep(0.2)
+    #Ouverture du driver chrome
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    time.sleep(0.2)
 
-	#Passer les cookies
-	try:
-		driver.find_element(By.ID, 'L2AGLb').click()
-	except:
-		print('No cookies pass needed')
-	time.sleep(0.2)
+    #Passer les cookies
+    try:
+        driver.find_element(By.ID, 'L2AGLb').click()
+    except:
+        print('No cookies pass needed')
+    time.sleep(0.2)
 
-	#Scraping SERP google result sur toutes les pages accessibles
-	max_page = 10
-	cpt_page = 0
+    #Scraping SERP google result sur toutes les pages accessibles
+    max_page = 10
+    cpt_page = 0
 
-	while cpt_page < max_page:
-		cpt_page += 1
-		link_scraper(links_list, driver)
+    while cpt_page < max_page:
+        cpt_page += 1
+        link_scraper(links_list, driver)
 
-		#Tente de passer a la page suivante
-		try:
-			driver.find_element(By.ID, value='pnnext').click()
-    	except:
-        	break
+        #Tente de passer a la page suivante
+        try:
+            driver.find_element(By.ID, value='pnnext').click()
+        except:
+            print("Fin de page")
+            break
         time.sleep(0.2)
-	
-	driver.quit()
+    
+    driver.quit()
