377981803bd6c736d178219124b3d89a1ce18894
Justin RUELLAND <justinruelland@icloud.com>
2023-06-26 21:32:16 +0200

    MaJ mails manager correction bugs

--- files_uw/mails_manager.py
+++ files_uw/mails_manager.py
@@ -5,8 +5,9 @@ def mails_finder(list_people, dict_company):
 	dict_unknown = {}
 	for cellule_cpt in range(list_people.len):
 		ptr_people = ptr_cell.people
+		ablaye = True
 		if ptr_people.email != 'x3':
-			continue
+			ablaye = False
 		company_get = ptr_people.company
 		company_transcript = company_get.lower().replace(" ", "")
 		if company_transcript in dict_company.keys():
@@ -14,10 +15,13 @@ def mails_finder(list_people, dict_company):
 				ptr_people.email = dict_company[company_transcript].replace("xfname", ptr_people.firstname).replace("xlname", ptr_people.lastname)
 			elif "yfname" in dict_company[company_transcript]: #Adresse mail du type j.smith@company.net
 				ptr_people.email = dict_company[company_transcript].replace("yfname", ptr_people.firstname[0]).replace("xlname", ptr_people.lastname)
-			continue
+			else: #Adresse mail unique
+				ptr_people.email = dict_company[company_transcript]
+			ablaye = False
 		if company_transcript in dict_unknown.keys():
-			continue
-		dict_unknown[company_transcript] = company_get
+			ablaye = False
+		if ablaye:
+			dict_unknown[company_transcript] = company_get
 		if ptr_cell.next != None:
 			ptr_cell = ptr_cell.next
 	if len(dict_unknown) > 0 :
@@ -34,3 +38,5 @@ def mail_finder(dictionnaire):
 			dictionnaire[company] = mail_struct.replace("John", "xfname").replace("Smith", 'xlname')
 		elif "F1N1L" in mail_struct:
 			dictionnaire[company] = mail_struct.replace("F1N1L", "yfname").replace("Smith", 'xlname')
+		else:
+			dictionnaire[company] = mail_struct
